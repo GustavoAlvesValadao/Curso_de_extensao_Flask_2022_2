a188b708dbd17bd0e3681b3edd2cb619579e67cd
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-08-10 22:02:20 -0300

    adicionando lista

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
     #criar uma variável com o meu nome
-    nome = "Gustavo Alves"
+    produtos = [
+        {"nome": "Ração", "preço": 199.99},
+        {"nome": "Playstation", "preço": 1999.99}
+    ]
     return render_template("alo.html", n = nome),200
 
 app.run()

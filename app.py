983ee520e4970844c5b26bd11b613b9c526bffcb
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-08-10 22:12:13 -0300

    adicionando a variável correta

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def ola_mundo():
         {"nome": "Ração", "preço": 199.99},
         {"nome": "Playstation", "preço": 1999.99}
     ]
-    return render_template("alo.html", n = nome, aProdutos = produto),200
+    return render_template("alo.html", n = nome, aProdutos = produtos),200
 
 app.run()

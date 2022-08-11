7204cbe1eda42d3f3d1ae913dabf8466f2dc8495
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-08-10 21:45:55 -0300

    atualizando o html

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
-    return render_template("alo.html"),200
+    #criar uma variável com o meu nome
+    nome = "Gustavo Alves"
+    return render_template("alo.html", n = nome),200
 
 app.run()

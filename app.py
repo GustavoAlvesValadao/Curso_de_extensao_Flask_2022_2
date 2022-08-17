ab13eaf7e7bc750aad38b1af229c0c969f328886
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-08-17 19:53:14 -0300

    adicionando rota alternativa

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from flask import Flask, render_template
 app = Flask("projeto")
+
+# Rota raiz
 @app.route("/")
 def ola_mundo():
     #criar uma variável com o meu nome
@@ -10,5 +12,11 @@ def ola_mundo():
         {"nome": "Playstation", "preço": 1999.99}
     ]
     return render_template("alo.html", n = nome, aProdutos = produtos),200
+
+# nova rota teste
+@app.route("/teste")
+@app.route("/teste/<variavel>")
+def funcao_teste(variavel = ""):
+    return "Nova rota teste<br>Variável: {}".format(variavel), 200
 
 app.run()
